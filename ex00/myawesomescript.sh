38378aaf339daa94f3f776e825f1bc527a0ee123
Dmitry Diordychuk <cort@mail.ru>
2021-10-25 14:16:00 +0300

    fix ex00

--- ex00/myawesomescript.sh
+++ ex00/myawesomescript.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # curl:
-# I - headers
 # s - silent
 # grep:
 # i - ignore case
-curl -Is $1 | grep -i Location | cut -f2,3 -d: | cut -f2 -d' '
+curl -s $1 | grep 'moved here' | cut -f2 -d\"
